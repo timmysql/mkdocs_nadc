40654856d1c28025cd88c022eb61d907cd4a1307
timmysql <timkonecky@outlook.com>
2022-12-27 16:57:53 -0600

    things

--- deploy.ps1
+++ deploy.ps1
@@ -1,11 +1,11 @@
 Set-Location "C:\Users\timko\code\mkdocs_nadc\"
 venv\Scripts\Activate
 
-# Set-Location "C:\Users\timko\code\mkdocs_nadc\" 
-# python.exe c:/Users/timko/code/mkdocs_nadc/database_daily_process.py
+Set-Location "C:\Users\timko\code\mkdocs_nadc\" 
+python.exe c:/Users/timko/code/mkdocs_nadc/database_daily_process.py
 
-# Set-Location "C:\Users\timko\code\mkdocs_nadc" 
-# python.exe c:/Users/timko/code/mkdocs_nadc/markdown_refresh_main.py
+Set-Location "C:\Users\timko\code\mkdocs_nadc" 
+python.exe c:/Users/timko/code/mkdocs_nadc/markdown_refresh_main.py
 
 # Set-Location "C:\Users\timko\code\mkdocs_nadc\nadc"
 # mkdocs gh-deploy
